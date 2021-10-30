8dbcd581e568045dc8895e4007ed6027636883b0
Erica Chen <ericachen70819@gmail.com>
2021-10-29 22:45:35 -0400

    adding new captions to image

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,14 +4,14 @@ import './Feed.css'
 
 const Feed = () => {
     const data = [{
-        name: "tester",
+        name: "Yellow Stone",
         image: "https://media.istockphoto.com/photos/grand-prismatic-spring-picture-id899846654?b=1&k=20&m=899846654&s=170667a&w=0&h=ON3z2xYiGOzuVEqfqb8yt9roHdWfCTnip8O5ck3yPTI=",
-        text: "tester",
+        text: "Look at this rainbow pattern!",
     },
     {
     name: "Yellow Stone",
     image: "https://media.istockphoto.com/photos/yellowstone-falls-river-grand-canyon-national-park-montana-mt-picture-id154931596?b=1&k=20&m=154931596&s=170667a&w=0&h=MI90LXLguHIkCqijs40tRngRwtr2QjzhrV7YG_BVRhw=",
-    text: "Splash",
+    text: "Water falls go plash!",
     },
     ]; // use []; when more than one entry in the array
     
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className="footer">
-            <p>Build with love during the Software Engineering Summit</p>
+            <p>Build with love by Erica chen during the Capital One Software Engineering Summit</p>
         </div>
     )
 }
