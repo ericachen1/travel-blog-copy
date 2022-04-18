0c8581a7150deddbf6334fa0854cf91575259a19
erica chen <ericachen70819@gmail.com>
2022-04-17 20:12:49 -0400

    new

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,60 +4,83 @@ import './Feed.css'
 
 const Feed = () => {
     const data = [{
-        name: "Un mapa de Corona, Queens",
-        image: "https://si.wsj.net/public/resources/images/NY-DT765_NYOPEN_16U_20151015190030.jpg",
-        text: "Este es el mapa de Corona, Queens. Corona es cuatrocientos trece (413.24) acres con dos millones cuatrocientos cinco mil cuatrocientos sesenta y cuatro (2,405,464) personas en 2020. Corona está al este de Jackson Heights y al oeste de Flushing. Corona está dos millas al norte de Forest Hills y seis al sur de East Elmhurst.",
+
+        type: "mural de vinilo",
+        name: "One Time Square",
+        image: "https://i0.wp.com/gothamtogo.com/wp-content/uploads/2019/08/Domingo-Zapata-Mural-in-Times-Square.jpeg?ssl=1",
+        title: "Life is a Dream",
+        artist: "Domingo Zapata",
+        text: " 33,000 square feet tall",
+        year: "2019",
+        style: "Moderno",
+        justification: "",
     },
     {
-        name: "Foto de una calle de los 1800 de Corona, Queens",
-        image: "https://www.bygonely.com/wp-content/uploads/2020/11/featured_Corona_Queens_New_York_City_1982.jpg",
-        text: "Esta es una imagen de la calle 108 en Corona, Queens. 108 calle en el centro de Corona, Queens. Este comunicado tiene un supermercado mexicano, una silla en frente al supermercado y un carro a la derecha del supermercado. Las casas están cerca del supermercado. Corona tiene muchos supermercados mexicanos.",
-    },
-    { 
-        name: "El mural y las actividades en la comunidad ",
-        image:"https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvYXNzZXRzLzQwM2M0OGVhLTk5MTAtNDAzZi1hZmQwLTIzYmU2NDA0MzgyZWQwMzY5NGQxNWQ2MjhiOTJlZl9HZXJhZGEtQXJ0LTIyMyBob3Jpem9udGFsLmpwZyJdLFsicCIsImNvbnZlcnQiLCIiXSxbInAiLCJjb252ZXJ0IiwiLXF1YWxpdHkgODEgLWF1dG8tb3JpZW50Il0sWyJwIiwidGh1bWIiLCIxMjgweD4iXV0/Gerada-Art-223%20horizontal.jpg",
-        text: "Este es un mural de un médico durante la pandemia de coronavirus. Este mural está en el parque de la Corona de Flushing Meadows. Queens tiene menos camas que hospital que Manhattan pero Queens tiene (el doble) más personas que Manhattan. Muchas Mucho más personas viven en Corona pero no hay pocos PPE y camas de hospital.",
-    },
-    {
-        name: "Fábrica Tiffany Abandonada",
-        image: "https://forgotten-ny.com/wp-content/uploads/2013/03/tiffany.jpg",
-        text: "Esta es la fábrica de lámparas de Tiffany en 43 avenida y 97 calle en Corona durante 1901 - 1932. Esta fábrica es roja y muy grande pero en la Gran Depresión la lámpara de Tiffany tiene menos negocios. En 2013, la fábrica está en construcción. Además, se construyó una nueva escuela llamada P.S. 315.         ",
-    },
-    {
-        name: "El museo Hall of Science",
-        image: "https://tile.loc.gov/image-services/iiif/service:pnp:highsm:31200:31263/full/pct:50/0/default.jpg",
-        text: "Esta es la sala del museo de ciencias . En cinco años, este museo cumplirá sesenta años. La sala del museo de ciencias tiene 400 exhibiciones sobre matemáticas, ciencia, biología, química y física. Este museo es uno de los primeros museos que visito. Yo recomiendo el museo de matemáticas en Midtown, Manhattan. Una visita para estudiantes cuesta 18 dólares para una entrada es de 18 dineros pero este museo es NGO. El museo de matemáticas y la sala del museo de ciencias son similares porque la exhibición es muy interactiva.         ",
-    },
-    {
-        name: "El Parque Flushing Meadows Corona",
-        image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/flushing-meadows-park-unisphere-christina-martinez.jpg",
-        text: "Este es el parque de Flushing Meadows Corona. En 1939 años, Nueva York tiene un World Fair en este parque. 44 millones hombres, mujeres y niños fueron World 's Fair. Me gusta este parque porque las plantas son bonitas en abril. Yo recomiendo el parque de Flushing Meadows Corona para picnics con tu familia.         ",
-    },
-    {
-        name: "El Cine",
-        image: "http://photos.cinematreasures.org/production/photos/141691/1446412120/large.png?1446412120",
-        text: "Esta es una imagen del teatro de Loew 's Plaza en el centro de Corona, Queens en National calle y Roosevelt Avenida en 1927. El Teatro de Loew 's Plaza es muy grande con dos pisos. Este teatro tiene dos mil ciento cincuenta y una (2,151) sillas. Pero en 2005, el teatro de Loew 's Plaza cerró. Por 78 años, muchas personas fueron al teatro a ver películas. Ahora este teatro es una farmacia Walgreens.         ",
+        type: "pintura al oleo de mujeres",
+        name: "Museo de Arte Moderno",
+        image: "https://www.pablopicasso.org/images/paintings/avignon.jpg",
+        title: "Les Demoiselles d'Avignon",
+        artist: "Pablo Picasso",
+        text: "https://prezi.com/view/4oq80IW6MyEFEUXSfLYr/",
+        year: "1907",
+        style: "Cubismo",
+        justification: "https://prezi.com/view/4oq80IW6MyEFEUXSfLYr/",
     },
 
     ]; // use []; when more than one entry in the array
     
     const [posts, setPosts] = useState(data); // data rep by useState variable 
+    
+    const [type, setType] = useState('');
     const [location, setLocation] = useState('');
     const [imgURL, setImgURL] = useState('');
+    const [title, setTitle] = useState('');
+    const [artist, setArtist] = useState('');
     const [text, setText] = useState('');
+    const [year, setYear] = useState('');
+    const [style, setStyle] = useState('');
+    const [justification, setJustification] = useState('');
 
+    const typeChangeHandler = (event) => {
+        setType(event.target.value)
+    }
     const locationChangeHandler = (event) => {
         setLocation(event.target.value)
     }
     const URLChangeHandler = (event) => {
         setImgURL(event.target.value)
     }
+    const titleChangeHandler = (event) => {
+        setTitle(event.target.value)
+    }
+    const artistChangeHandler = (event) => {
+        setArtist(event.target.value)
+    }
     const textChangeHandler = (event) => {
         setText(event.target.value)
     }
+    const yearChangeHandler = (event) => {
+        setYear(event.target.value)
+    }
+    const styleChangeHandler = (event) => {
+        setStyle(event.target.value)
+    }
+    const justificationChangeHandler = (event) => {
+        setJustification(event.target.value)
+    }
 
     const addPosts = () => {
-        const newPost = {name:location, image:imgURL, text:text}
+        const newPost = {
+            type: type,
+            name: location,
+            image: imgURL,
+            title: title,
+            artist: artist,
+            text: text,
+            year: year,
+            style: style,
+            justification: justification,    
+        }
         console.log(newPost);
         setPosts([newPost, ...posts]) // new posts and everything else in the post
         console.log(posts)
@@ -67,17 +90,49 @@ const Feed = () => {
         <div className="feed">
             <div className="newPost">
                 <h2>Add new post</h2>
-                <label>Location</label>
+
+                <label>Tipo de obra</label>
+                <input onChange={typeChangeHandler}></input>
+
+                <label>Localización</label>
                 <input onChange={locationChangeHandler}></input>
-                <label>Image URL</label>
+
+                <label>Imagen</label>
                 <input onChange={URLChangeHandler}></input>
-                <label>Text</label>
+
+                <label>Título de la obra</label>
+                <input onChange={titleChangeHandler}></input>
+                
+                <label>Artista</label>
+                <input onChange={artistChangeHandler}></input>
+                
+                <label>Descripción</label>
                 <input onChange={textChangeHandler}></input>
+
+                <label>Año</label>
+                <input onChange={yearChangeHandler}></input>
+                
+                <label>Estilo</label>
+                <input onChange={styleChangeHandler}></input>
+
+                <label>Justificación</label>
+                <input onChange={justificationChangeHandler}></input>
+                
             </div>
             <button onClick={addPosts}>Add post</button>
 
             {posts.map((location) => (
-                <Post name={location.name} image={location.image} text={location.text}/>
+                <Post 
+                name={location.name} 
+                image={location.image} 
+                text={location.text}
+                type={location.type}
+                title={location.title}
+                artist={location.artist}
+                year={location.year}
+                style={location.style}
+                justification={location.justification}
+                />
             ))}
         </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,8 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header">
-            <h1>Blog de Viajes Corona, Nueva York</h1>
+            <h1>Blog de Viajes Arte Español en Nueva York</h1>
             <p>explora esta pueblo en Queens conmigo</p>
-            <p>Bienvenidos, yo soy Erica. Este es mi proyecto para la clase de español 1101 Español Proyecto. Vivo en Queens, Nueva York. Queens tiene muchas comunidades. Astoria, Jackson Heights, Long Island City y Sunnyside están en el noroeste de Queens. Bayside, Bellerose, College Point, Little Neck, Flushing, Pomonok, Floral Park, Fresh Meadows, Glen Oaks y Whitestone están el noreste de Queens. Briarwood, Corona, LeFrak City (una urbanización), North Corona, East Elmhurst, Elmhurst, Forest Hills, Forest Hills Gardens, Fresh Pond, Glendale, Jackson Heights, Kew Gardens, Maspeth, Middle Village, Rego Park, Ridgewood, Wyckoff Heights y Woodside están el centro de Queens. Mi proyecto es sobre Corona, Queens. Corona, Queens tiene una historia única. Esta ciudad tiene el mismo nombre que Crown Building Company. Queens tiene dos millones dos mil doce trescientos sesenta (2,212,360) personas en 2019.
-</p>
         </div>
     )
 }
