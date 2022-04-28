05f787285d4d12a1c65484357348c1f240207893
erica chen <ericachen70819@gmail.com>
2022-04-27 20:06:33 -0400

    testing AGAIN

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header">
+            <p>HELLO TESTING</p>
             <h1>Blog de Viajes Arte Español en Nueva York</h1>
             <p>explora esta pueblo en Queens conmigo</p>
         </div>
